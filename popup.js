0b6cd1922ddb6817ddb0ec21cfc582ecd527f553
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-12-10 11:29:21 -0800

    WIP

--- popup.js
+++ popup.js
@@ -1,20 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
+document.addEventListener("DOMContentLoaded", function() {
+  var signUp = document.getElementById("sign-up");
+  var signIn = document.getElementById("sign-in");
+  var startContainer = document.getElementById("start-container");
+  var signInContainer = document.getElementById("sign-in-container");
+  var signInForm = document.getElementById("sign-in-form");
+  var signInSubmit = document.getElementById("sign-in-submit");
 
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
+  signUp.addEventListener("click", function() {
+    chrome.tabs.create({url: "https://link-savr.herokuapp.com"});
+  });
 
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
+  signIn.addEventListener("click", function() {
+    startContainer.style.display = "none";
+    signInContainer.style.display = "block";
+  });
+
+  signInSubmit.addEventListener("click", function() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    $.ajax({
+      dataType: "json",
+      url: "http://localhost:3000/auth/login",
+      method: "POST",
+      data: {email: email, password: password}
+    }).done(function(data) {
+      console.log(data)
     });
-  }, false);
+  });
+
 }, false);
+
+
+// https://www.google.com/search?q=build+chrome+extension&oq=build+chrome+e&aqs=chrome.0.0l2j69i57j0l3.2776j0j7&sourceid=chrome&ie=UTF-8
